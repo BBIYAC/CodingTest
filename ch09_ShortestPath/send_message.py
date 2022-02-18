0c5d54853b6958500e75759b0c165d45d0e90a81
inyeong <kiy7833@gmail.com>
2022-02-18 21:20:58 +0900

    Update send message

--- ch09_ShortestPath/send_message.py
+++ ch09_ShortestPath/send_message.py
@@ -19,19 +19,24 @@
 
 # 출력 예시
 2 4
+
+# heapq
+힙큐는 가장 작은 값을 리스트 맨 앞에 추가하는 형식
 '''
 import heapq
 
 # 도시의 개수 n, 통로의 개수 m, 메시지를 보내고자 하는 도시 c
 n, m, c = map(int, input('>> ').split())
+c = c-1
 # 2차원 배열 빈 테이블로 초기화
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n)]
 # 최단 거리 테이블을 모두 무한으로 초기화
 INF = int(1e9)
-distance = [INF]*(n+1)
+distance = [INF]*(n)
 # 통로에 대한 정보 x, y, z
 for _ in range(m):
     x, y, z = map(int, input('>> ').split())
+    x, y = x-1, y-1
     # x번 노드에서 y번 노드로 가는 비용이 z
     graph[x].append((y, z))
 
@@ -40,11 +45,11 @@ def solution():
     # 빈 큐 생성
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하며, 큐에 삽입
-    heapq.heappush(q, (0, c))
+    heapq.heappush(q, (c, 0))
     distance[c] = 0
     while q: # 큐가 비어있지 않다면
         # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
+        now, dist = heapq.heappop(q)
         if distance[now] < dist:
             continue
         # 현재 노드와 연결된 다른 인접한 노드 확인
@@ -53,7 +58,7 @@ def solution():
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q, (i[0], cost))
 
     # 도달할 수 있는 노드(도시) 개수
     count = 0
