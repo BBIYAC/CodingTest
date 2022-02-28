571ed00e1f3554b1a194506bdd968243f198f62c
inyeong <kiy7833@gmail.com>
2022-02-28 21:28:23 +0900

    Add answer string rearrangement

--- ch04_Implementation/string_rearrangement.py
+++ ch04_Implementation/string_rearrangement.py
@@ -35,7 +35,36 @@ def solution():
             num += int(s)
         else: # 문자면 정답에 +
             answer += s
-    answer += str(num)
+    if num != 0:
+        answer += str(num)
     return answer
 
 print(solution())
+
+
+# 정답
+def answer_solution():
+    data = input('>> ')
+    result = []
+    value = 0
+
+    # 문자 하나씩 확인
+    for x in data:
+        # 알파벳인 경우 결과 리스트에 삽입
+        if x.isalpha():
+            result.append(x)
+        # 숫자는 따로 더하기
+        else:
+            value += int(x)
+
+    # 알파벳을 오름차순으로 정렬
+    result.sort()
+
+    # 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+    if value != 0:
+        result.append(str(value))
+
+    # 최정 결과 출력
+    return ''.join(result)
+
+print(answer_solution())    
