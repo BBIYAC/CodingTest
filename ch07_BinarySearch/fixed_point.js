2d8b3a56ee37129d94ef5d3377b705885241ce51
inyeong <kiy7833@gmail.com>
2022-03-25 19:40:56 +0900

    Add ansewr fixed point

--- ch07_BinarySearch/fixed_point.js
+++ ch07_BinarySearch/fixed_point.js
@@ -40,4 +40,42 @@ function solution(n, array){
     return answer
 }
 
-console.log(solution(7, [-15, -4, 3, 8, 9, 13, 15]))
+console.log(solution(7, [-15, -4, 2, 8, 9, 13, 15]))
+
+// 정답
+function binary_search(array, start, end){
+    if (start > end){
+        return -1;
+    }
+    mid = (start+end) / 2
+    // 고정점을 찾은 경우 인덱스 반환
+    if (array[mid] == mid){
+        return mid;
+    }
+    // 중간점이 가리키는 위치의 값보다 중간점이 작은 경우 왼쪽 확인
+    else if (array[mid] > mid){
+        return binary_search(array, start, mid-1);
+    }
+    // 중간점이 가리키는 위치의 값보다 중간점이 큰 경우 오른쪽 확인
+    else{
+        return binary_search(array, mid+1, end);
+    }
+}
+
+function answer_solution(n, array){
+    var answer = 0;
+
+    // 이진탐색 수행
+    var index = binary_search(array, 0, n-1);
+
+    // 고정점이 없는 경우 -1 출력
+    if (index == -1){
+        answer = -1;
+    }else{ // 고정점이 있는 경우 해당 인덱스 출력
+        answer = index;
+    }
+    return answer
+}
+
+
+console.log(answer_solution(7, [-15, -4, 2, 8, 9, 13, 15]))
