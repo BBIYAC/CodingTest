9b77af5c8b752dbbcf506f716468e8146335b136
inyeong <kiy7833@gmail.com>
2022-05-13 21:45:42 +0900

    :pencil: Add remain 1.js

---
+++ Programmers/remain_1.js
@@ -0,0 +1,10 @@
+/* 나머지가 1이 되는 수 찾기 */
+function solution(n) {
+    var answer = 1e9;
+    for(var i=0; i<n; i++){
+        if(n%i == 1){
+            answer = Math.min(answer, i);
+        }
+    }
+    return answer;
+}
