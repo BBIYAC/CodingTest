91a09070e9a5b3b7afee321de315ebae8a152345
inyeong <kiy7833@gmail.com>
2022-05-19 19:55:06 +0900

    :pencil: Add string_sort_desc.js

---
+++ Programmers/string_sort_desc.js
@@ -0,0 +1,12 @@
+/* 문자열 내림차순 정렬 */
+function solution(s) {
+    var arr = [];
+    for(var char of s){
+        arr.push(char);
+    }
+    arr.sort((a,b)=>{
+        if(a<b) return 1;
+        if(a>b) return -1;
+        if(a===b) return 0;})
+    return arr.join('');
+}
