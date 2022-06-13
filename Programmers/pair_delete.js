03bf9290e1f82637dacb38650ac115d2bd7cecc4
inyeong <kiy7833@gmail.com>
2022-06-14 00:04:54 +0900

    :pencil2: Add other answer pair_delete.js

--- Programmers/pair_delete.js
+++ Programmers/pair_delete.js
@@ -1,13 +1,24 @@
 /* 짝지어 제거하기 */
-function solution(s){
-    const strArr = s.split("");
+// function solution(s){
+//     const strArr = s.split("");
 
-      for(let i = 0 ; i < strArr.length-1 ; i++){
-        if(strArr[i] === strArr[i+1]){
-            strArr.splice( i, 2 );
-              i = -1;
-        }
+//       for(let i = 0 ; i < strArr.length-1 ; i++){
+//         if(strArr[i] === strArr[i+1]){
+//             strArr.splice( i, 2 );
+//               i = -1;
+//         }
+//     }
+
+//   return strArr.length === 0 ? 1 : 0;
+// }
+
+function solution(s) {
+    const stack = [];
+
+      for(let i = 0 ; i < s.length ; i++ ){
+        if( !stack.length || stack[stack.length-1] !== s[i] ) stack.push(s[i]);
+        else stack.pop();
     }
 
-  return strArr.length === 0 ? 1 : 0;
+  return stack.length ? 0 : 1;
 }
