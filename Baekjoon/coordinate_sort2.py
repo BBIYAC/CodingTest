63740350245fc45bc7678bad4c409c80ebad603f
BBIYAC <kiy7833@gmail.com>
2022-07-29 14:50:29 +0900

    :pencil2: Add coordinate_sort2.py

---
+++ Baekjoon/coordinate_sort2.py
@@ -0,0 +1,31 @@
+# 좌표 정렬하기 2
+'''
+문제
+2차원 평면 위의 점 N개가 주어진다.
+ 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 
+둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. 
+(-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+# 점의 개수 N
+n = int(input().rstrip())
+# 점 리스트
+n_list = [0] * n
+for i in range(n):
+    n_list[i] = tuple(map(int, input().split())) # (xi, yi)
+
+# 정렬 (y좌표 오름차순 정렬, y좌표 같으면 x좌표 오름차순 정렬)
+n_list.sort(key = lambda x: (x[1], x[0]))
+
+# 출력
+for x, y in n_list:
+    print(x, y)
