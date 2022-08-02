cf81f2b7d8fe6dec3aa34c390a7891f92f73c978
BBIYAC <kiy7833@gmail.com>
2022-08-02 15:09:18 +0900

    :pencil2: Add n_and_m2.py

---
+++ Baekjoon/n_and_m2.py
@@ -0,0 +1,36 @@
+# N과 M 2
+'''
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+1. 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+2. 고른 수열은 오름차순이어야 한다.
+
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 
+중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+'''
+
+import sys
+n, m = map(int, sys.stdin.readline().split())
+nums, answer = list(), list()
+ 
+def nm():
+    if len(nums)==m:
+        if sorted(nums) not in answer: 
+            answer.append(sorted(nums))
+        return
+
+    for i in range(1,n+1):
+        if i not in nums:
+            nums.append(i)
+            nm()
+            nums.pop()
+ 
+nm()
+
+for num in answer:
+    print(*num)
