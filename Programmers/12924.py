2e49b7998f430f3f7dd607525d5693ca84fb61d4
BBIYAC <kiy7833@gmail.com>
2022-11-24 20:59:58 +0900

    fix(12924): 시간 초과 에러 해결

--- Programmers/12924.py
+++ Programmers/12924.py
@@ -7,3 +7,19 @@ def solution(n):
             if sum(numArray[i-1:j]) == n:
                 answer += 1
     return answer
+
+# 시간 초과 에러 해결
+def solution(n):
+    answer = 0
+    numArray = list(range(1, n + 1))
+    i, j = 0, 0
+    while i < n:
+        sumResult = sum(numArray[i:j])
+        if sumResult > n:
+            i += 1
+        elif sumResult < n:
+            j += 1
+        else:
+            answer += 1
+            i += 1
+    return answer
