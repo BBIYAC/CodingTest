28976097bb2b685f06c0acd6744f9adb65ebb808
BBIYAC <kiy7833@gmail.com>
2022-12-05 18:29:49 +0900

    resolve: 에상 대진표 다른 해결법

--- Programmers/12985.py
+++ Programmers/12985.py
@@ -13,3 +13,11 @@ def solution(n,a,b):
   return answer
 
 print(solution(8, 5, 4)) # 3
+
+def other_solution(n, a, b):
+  answer = 0
+  while a != b:
+      answer += 1
+      a, b = (a+1)//2, (b+1)//2
+
+  return answer
