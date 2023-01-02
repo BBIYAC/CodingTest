04a13100eecf648fc22fea5db407be1e3ce26346
BBIYAC <kiy7833@gmail.com>
2023-01-02 23:11:38 +0900

    solve: 크기가 작은 부분 문자열

---
+++ Programmers/147355.py
@@ -0,0 +1,8 @@
+# [프로그래머스] 크기가 작은 부분 문자열
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1):
+        if int(t[i:i+len(p)]) <= int(p):
+            answer += 1
+    
+    return answer
