40e1f5ec4fd3c81d9b16102df6c7650763e817b2
BBIYAC <kiy7833@gmail.com>
2023-03-22 23:05:48 +0900

    resolve: DNA

--- Baekjoon/1969.py
+++ Baekjoon/1969.py
@@ -1,5 +1,46 @@
 # [백준 1969] DNA
 
+# import sys
+# input = sys.stdin.readline
+
+# # DNA의 수 N, 문자열의 길이 M
+# N, M = map(int, input().split())
+# DNA = [input().rstrip() for _ in range(N)]
+
+# # 최소 Hamming Distance
+# HD = 0
+# HD_DNA = ''
+
+# for i in range(M):
+#     ACGT = [0, 0, 0, 0]
+#     for j in range(N):
+#         if DNA[j][i] == 'A':
+#             ACGT[0] += 1
+#         elif DNA[j][i] == 'C':
+#             ACGT[1] += 1
+#         elif DNA[j][i] == 'G':
+#             ACGT[2] += 1
+#         elif DNA[j][i] == 'T':
+#             ACGT[3] += 1
+    
+#     # 가장 많이 나온 문자 인덱스
+#     max_idx = ACGT.index(max(ACGT))
+
+#     if max_idx == 0:
+#         HD_DNA += 'A'
+#     elif max_idx == 1:
+#         HD_DNA += 'C'
+#     elif max_idx == 2:
+#         HD_DNA += 'G'
+#     elif max_idx == 3:
+#         HD_DNA += 'T'
+#     # N - 가장 많이 나온 문자 수
+#     HD += N - max(ACGT)
+
+# print(HD_DNA)
+# print(HD)
+
+import operator
 import sys
 input = sys.stdin.readline
 
@@ -12,30 +53,13 @@ HD = 0
 HD_DNA = ''
 
 for i in range(M):
-    ACGT = [0, 0, 0, 0]
+    ACGT = {'A': 0, 'C': 0,'G': 0, 'T': 0}
     for j in range(N):
-        if DNA[j][i] == 'A':
-            ACGT[0] += 1
-        elif DNA[j][i] == 'C':
-            ACGT[1] += 1
-        elif DNA[j][i] == 'G':
-            ACGT[2] += 1
-        elif DNA[j][i] == 'T':
-            ACGT[3] += 1
+        ACGT[DNA[j][i]] += 1
     
-    # 가장 많이 나온 문자 인덱스
-    max_idx = ACGT.index(max(ACGT))
-
-    if max_idx == 0:
-        HD_DNA += 'A'
-    elif max_idx == 1:
-        HD_DNA += 'C'
-    elif max_idx == 2:
-        HD_DNA += 'G'
-    elif max_idx == 3:
-        HD_DNA += 'T'
-    # N - 가장 많이 나온 문자 수
-    HD += N - max(ACGT)
+    max_DNA = max(ACGT.items(), key=operator.itemgetter(1))
+    HD_DNA += max_DNA[0] # 가장 많이 나온 문자
+    HD += N - max_DNA[1] # N - 가장 많이 나온 문자 수
 
 print(HD_DNA)
 print(HD)
