aba6f6fc07b069f798d1d1cf33e344e53378dd09
BBIYAC <kiy7833@gmail.com>
2023-03-27 21:29:18 +0900

    refactor: 두 스티커

--- Baekjoon/16937.py
+++ Baekjoon/16937.py
@@ -10,23 +10,22 @@ N = int(input().rstrip())
 # 스티커의 크기 Ri, Ci
 stickers = [list(map(int, input().split())) for _ in range(N)]
 
+# 4가지 케이스 테스트 함수
+def isCase(a, b, c, d):
+    return (a+b <= H and max(c, d) <= W) or (max(a, b) <= H and c+d <= W)
+
 answer = 0
 for i in range(N):
     for j in range(i+1, N):
         R1, C1 = stickers[i]
         R2, C2 = stickers[j]
 
-        # 둘 다 회전하지 않을 경우
-        if (R1+R2 <= H and max(C1, C2) <= W) or (max(R1, R2) <= H and C1+C2 <= W):
-            answer = max(answer, R1*C1 + R2*C2)
-        # 첫번째 스티커만 회전하는 경우
-        if (C1+R2 <= H and max(R1, C2) <= W) or (max(C1, R2) <= H and R1+C2 <= W):
-            answer = max(answer, R1*C1 + R2*C2)
-        # 두번째 스티커만 회전하는 경우
-        if (R1+C2 <= H and max(C1, R2) <= W) or (max(R1, C2) <= H and C1+R2 <= W):
-            answer = max(answer, R1*C1 + R2*C2)
-        # 둘 다 회전하는 경우
-        if (C1+C2 <= H and max(R1, R2) <= W) or (max(C1, C2) <= H and R1+R2 <= W):
+        true_case = [isCase(R1, R2, C1, C2), # 둘 다 회전하지 않을 경우
+                     isCase(C1, R2, R1, C2), # 첫번째 스티커만 회전하는 경우
+                     isCase(R1, C2, C1, R2), # 두번째 스티커만 회전하는 경우
+                     isCase(C1, C2, R1, R2)] # 둘 다 회전하는 경우
+        
+        if sum(true_case) > 0:
             answer = max(answer, R1*C1 + R2*C2)
 
 print(answer)
